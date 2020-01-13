0cab980d9b17a29febf05fc1ada15e61afe92c03
Markus Röman <markus.roman@hotmail.fi>
2020-01-13 18:23:11 +0200

    Signup component done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Paperbase from "./components/Paperbase";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/signup" render={() => <SignUp />} />
         <Route exact path="/" render={() => <Paperbase />} />
       </Router>
     </div>
