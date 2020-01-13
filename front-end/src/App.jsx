34184d352f8f1de958af20c8d77a468669c216ea
JonesTPG <joonas.ryynanen1@gmail.com>
2020-01-13 10:24:49 +0200

    initial front-end commit.

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>es</p>
+    </div>
+  );
+};
+
+export default App;
