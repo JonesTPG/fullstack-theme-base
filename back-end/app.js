10f390d05151dd9b1b74e886d2e66a846f573d98
JonesTPG <joonas.ryynanen1@gmail.com>
2020-02-02 13:36:12 +0200

    subscription url testing.

--- back-end/app.js
+++ back-end/app.js
@@ -39,7 +39,8 @@ app.use(bodyParser.json());
 
 const apolloServer = new ApolloServer({
   schema,
-  context
+  context,
+  playground: {}
 });
 
 apolloServer.applyMiddleware({ app });
--- back-end/index.js
+++ back-end/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
       },
       {
         server: localSslServer,
-        path: '/graphql'
+        path: '/subscriptions'
       }
     );
   });
