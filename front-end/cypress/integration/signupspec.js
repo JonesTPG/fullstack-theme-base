254f19addf03d5492f1f8ef4269e68506fcde5c3
JonesTPG <joonas.ryynanen1@gmail.com>
2020-02-03 15:46:00 +0200

    cypress: env variable files. refactored urls in tests.

--- front-end/cypress/integration/signupspec.js
+++ front-end/cypress/integration/signupspec.js
@@ -8,10 +8,10 @@ describe('Fullstack-theme-base sign up page ', function() {
       it('User can create a new account in sign up page', function() {
         cy.request({
           method: 'POST',
-          url: 'http://localhost:4000/graphql/',
+          url: Cypress.env('BASE_SERVER_URL') + '/graphql/',
           body: resetDatabase
         });
-        cy.visit(Cypress.env('BASE_URL') + '/login');
+        cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
         cy.contains("Don't have an account? Sign Up");
         cy.get('[data-cy=signUp]').click();
         cy.contains('Sign up');
@@ -25,24 +25,24 @@ describe('Fullstack-theme-base sign up page ', function() {
     });
     describe('User can log in with created creadentials', function() {
       it('User can create a new account in sign up page', function() {
-        cy.visit(Cypress.env('BASE_URL') + '/login');
+        cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
         cy.contains('Sign in');
         cy.get('[data-cy=username]').type('username');
         cy.get('[data-cy=password]').type('password');
         cy.contains('Sign in').click();
         cy.get('[data-cy=signIn]').click();
-        cy.url().should('eq', Cypress.env('BASE_URL') + '/');
+        cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/');
       });
     });
     describe('Trying to log in with invalid creadentials', function() {
       it("User can't log in with non-existing credentials", function() {
-        cy.visit(Cypress.env('BASE_URL') + '/login');
+        cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
         cy.contains('Sign in');
         cy.get('[data-cy=username]').type('non-existingUsername');
         cy.get('[data-cy=password]').type('non-existingPassword');
         cy.contains('Sign in').click();
         cy.get('[data-cy=signIn]').click();
-        cy.url().should('eq', Cypress.env('BASE_URL') + '/login');
+        cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/login');
       });
     });
   });
--- front-end/cypress/plugins/index.js
+++ front-end/cypress/plugins/index.js
@@ -11,7 +11,18 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require('fs');
+const path = require('path');
+
+const getConfigurationByFile = file => {
+  const pathToConfigFile = path.resolve('.', 'cypress/config', `${file}.json`);
+  const rawData = fs.readFileSync(pathToConfigFile);
+  return JSON.parse(rawData);
+};
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  const file = config.env.configFile || 'development';
+  return getConfigurationByFile(file);
+};
