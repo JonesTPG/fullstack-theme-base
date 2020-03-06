c5cd9c3d722f140c5819bd5e06ee1018d49af247
JonesTPG <joonas.ryynanen1@gmail.com>
2020-03-06 10:40:45 +0200

    initial project service functions.

---
+++ back-end/services/project.js
@@ -0,0 +1,26 @@
+/**
+ * Service that calculates the new prices for projects based on the amount of customer that are commited to buy the project.
+ */
+
+/**
+ * Returns information about the project object based on project id.
+ * @param {string} projectId
+ */
+const getProjectDetails = projectId => {
+  console.log('get details');
+};
+
+/**
+ * Save the updated project object to the database. Possibly perform additional actions.
+ * @param {Project} projectData
+ */
+const updateProjectDetails = projectData => {
+  console.log('update details');
+};
+
+/**
+ * Calculate the updated project information
+ */
+const calculateDiscount = projectId => {
+  console.log('discount');
+};
