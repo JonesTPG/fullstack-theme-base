6e42b6d0d3325fb2ff7883ab6f6c0a98bcc02283
JonesTPG <joonas.ryynanen1@gmail.com>
2020-03-08 15:49:08 +0200

    ecommerce related database data.

---
+++ example-data/database/features.js
@@ -0,0 +1,20 @@
+const features = [
+  {
+    name: "Authentication",
+    description:
+      "Authentication system that allows users to make an account and save their orders and other data.",
+    imgUrl: "authentication.jpg"
+  },
+  {
+    name: "Modern payment methods",
+    description:
+      "User authentication system that allows for easy access control",
+    imgUrl: "payment.jpg"
+  },
+  {
+    name: "Different shipping methods",
+    description:
+      "Offer shipping methods based on customer location, product attributes and other variables.",
+    imgUrl: "shipping.jpg"
+  }
+];
---
+++ example-data/database/projects.js
@@ -0,0 +1,79 @@
+const eCommerceProjects = [
+  {
+    name: "WooCommerce",
+    description:
+      "WooCommerce is now the most popular eCommerce platform on the web, so you can rest assured you're in good company.",
+    features: [],
+    price: 1000000,
+    currentPrice: 1000000,
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  },
+
+  {
+    name: "Builder.ai",
+    description:
+      "Online retailer and marketplace with a huge variety of products. Features include barcode scanning, user reviews, save and compare and order tracking.",
+    features: [],
+    price: 45000000,
+    currentPrice: 450000000,
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  },
+
+  {
+    name: "ReFox ERP & WebShop",
+    description:
+      "One of the only softwares that has all the necessary tools a company can want in one package: ERP, webstore, inventory management etc.",
+    features: [],
+    price: 6000000,
+    currentPrice: 6000000,
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  }
+];
+
+const normalProjects = [
+  {
+    name: "CrushFTP",
+    description: "Robust SFTP Server for Enterprise needs",
+    features: [],
+    price: "1000",
+    currentPrice: "1000",
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  },
+
+  {
+    name: "Buddy.works",
+    description:
+      "The simplest CI/CD tool ever made recommended by top developers worldwide.",
+    features: [],
+    price: "300",
+    currentPrice: "300",
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  },
+
+  {
+    name: "Studio 3T",
+    description:
+      "Studio 3T is the professional GUI, IDE & client for MongoDB available for Windows, Mac, and Linux.",
+    features: [],
+    price: "600",
+    currentPrice: "600",
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  },
+
+  {
+    name: "F-Secure Key",
+    description:
+      "F‑Secure KEY protects and remembers all your pass­words and pin codes",
+    features: [],
+    price: "100",
+    currentPrice: "100",
+    participants: ["5e625acaa428c761fc9b33b5"],
+    endTime: 1583999027
+  }
+];
