2c2c00baa3a31546a005d2569e8b3f0701912aac
JanettaH <janetta.huoponen@gmail.com>
2020-03-26 16:20:25 +0200

    Fixed test files.

---
+++ front-end/cypress/integration/log_in.spec.js
@@ -0,0 +1,89 @@
+const resetDatabase = {
+  query: 'mutation {resetDatabase}'
+};
+
+const createUser = {
+  query:
+    'mutation {createUser(username:"username@gmail.com", password:"password"){username}}'
+};
+const createAdminUser = {
+  query: 'mutation {createAdminUser(username:"admin@gmail.com"){username}}'
+};
+
+describe('Full-stack-theme-base login', function() {
+  beforeEach(function() {
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('BASE_SERVER_URL') + '/graphql/',
+      body: resetDatabase
+    });
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('BASE_SERVER_URL') + '/graphql/',
+      body: createUser
+    });
+    cy.request({
+      method: 'POST',
+      url: Cypress.env('BASE_SERVER_URL') + '/graphql/',
+      body: createAdminUser
+    });
+  });
+  it('Admin page will be opened when logging in as a admin', function() {
+    cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
+    cy.contains('Sign in');
+    cy.get('[data-cy=email]').type('admin@gmail.com');
+    cy.get('[data-cy=password]').type('admin');
+    cy.contains('Sign in').click();
+    cy.get('[data-cy=signIn]').click();
+    cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/admin');
+  });
+
+  it("User can't login with invalid credentials", function() {
+    cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
+    cy.contains('Sign in');
+    cy.get('[data-cy=email]').type('innvalidEmail@gmail.com');
+    cy.get('[data-cy=password]').type('InvalidPassword');
+    cy.contains('Sign in');
+    cy.get('[data-cy=signIn]').click();
+    cy.contains('*The username or password you entered is incorrect.');
+    cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/login');
+  });
+
+  it('Front page can be opened', function() {
+    cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
+    cy.contains('Sign in');
+    cy.get('[data-cy=email]').type('username@gmail.com');
+    cy.get('[data-cy=password]').type('password');
+    cy.contains('Sign in').click();
+    cy.get('[data-cy=signIn]').click();
+    cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/');
+  });
+
+  it('Theme-base-token can be found in local storage after login', function() {
+    cy.clearLocalStorage('theme-base-token');
+    cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
+    cy.contains('Sign in');
+    cy.get('[data-cy=email]').type('username@gmail.com');
+    cy.get('[data-cy=password]').type('password');
+    cy.contains('Sign in').click();
+    cy.get('[data-cy=signIn]')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme-base-token')).to.include('ey');
+      });
+  });
+  it('Theme-base-token will be deleted after logout', function() {
+    cy.clearLocalStorage('theme-base-token');
+    cy.visit(Cypress.env('BASE_REACT_URL') + '/login');
+    cy.contains('Sign in');
+    cy.get('[data-cy=email]').type('username@gmail.com');
+    cy.get('[data-cy=password]').type('password');
+    cy.get('[data-cy=signIn]').click();
+    cy.url().should('eq', Cypress.env('BASE_REACT_URL') + '/');
+    cy.get('[data-cy=logout]')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('theme-base-token')).to.be.null;
+      });
+  });
+});
